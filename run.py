d1157072bc13f39459c19e192ef8f36e38f3d7f0
kmelve <knut@netliferesearch.com>
2016-12-01 17:28:13 +0100

    wip

--- run.py
+++ run.py
@@ -8,11 +8,17 @@ import re
 
 import os
 import psycopg2
-'''
-import urlparse
+import urllib.parse
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+urllib.parse.uses_netloc.append("mysql")
+url = urllib.parse.urlparse(DATABASE_URL)
 
 conn = psycopg2.connect(
     database=url.path[1:],
@@ -21,12 +27,13 @@ conn = psycopg2.connect(
     host=url.hostname,
     port=url.port
 )
-'''
+cursor = conn.cursor()
+
 def dateifyer(dateString):
     return datetime.strptime(str(dateString), '%d.%m.%y')
 
-def post_to_slack(url, r):
-    payload = u'Noe nytt har skjedd på SKAM: ' + url
+def post_to_slack(tittel, url, r):
+    payload = u'Noe nytt har skjedd på SKAM: <' + url + '|'+tittel+'>'
     slack_data = {'text': payload}
     response = r.post(
         WEBHOOK_URL, data=json.dumps(slack_data),
@@ -48,18 +55,13 @@ page = r.get('http://skam.p3.no')
 content = page.content
 soup = bs(content, "html.parser")
 articles = soup.find_all('article')
-articles[0].select('img')[0]
-bylines = [byline.string for byline in soup.select('.byline a')]
+bylines = [byline.text.replace('\n','') for byline in soup.select('.byline')]
 link = articles[0].find_all('a', href=True)[0]['href']
-post_to_slack(link, r)
-'''
-dateRegex = r".*(\d{2}\.\d{2}\.\d{2}).*"
-dates = [re.match(dateRegex, date).group(1) for date in bylines]
-if [row for row in c.execute("SELECT * FROM bylines LIMIT 1")]:
-    latestDate = [row for row in c.execute("SELECT * FROM bylines LIMIT 1")][0]
-    latestDate = dateifyer(latestDate[0])
-    latestDate > dateifyer(dates[0])
-    if latestDate > dateifyer(dates[0]):
-        c.execute("INSERT INTO bylines VALUES (?)", [dates[0]])
+
+cursor.execute("SELECT * FROM updates LIMIT 1")
+records = cursor.fetchall()
+if records:
+    if records[0][1] != bylines[0]:
+        post_to_slack(bylines[0], link, r)
+        cursor.execute("INSERT INTO updates (title) VALUES (%s)", [bylines[0]])
         conn.commit()
-'''
