d77a5a979771b384ff81c3ff7cef04031c06abc2
kmelve <knut@netliferesearch.com>
2016-12-02 10:20:36 +0100

    fixed datechecker

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import itertools
 import json
 import requests as r
 import re
-
 import os
 import psycopg2
 import urllib.parse
@@ -44,7 +43,6 @@ def post_to_slack(tittel, url, r):
             % (response.status_code, response.text)
         )
 
-
 page = r.get('http://skam.p3.no')
 content = page.content
 soup = bs(content, "html.parser")
@@ -52,10 +50,12 @@ articles = soup.find_all('article')
 bylines = [byline.text.replace('\n','') for byline in soup.select('.byline')]
 link = articles[0].find_all('a', href=True)[0]['href']
 
-cursor.execute("SELECT * FROM updates LIMIT 1")
+cursor.execute("SELECT * FROM updates ORDER BY ID DESC")
 records = cursor.fetchall()
 if records:
     if records[0][1] != bylines[0]:
-        post_to_slack(bylines[0], link, r)
+        # post_to_slack(bylines[0], link, r)
         cursor.execute("INSERT INTO updates (title) VALUES (%s)", [bylines[0]])
         conn.commit()
+    else:
+        print('Ingenting nytt.')
